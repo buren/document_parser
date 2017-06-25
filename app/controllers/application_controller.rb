250bce9195fb80db3a1393f39bec63fcf8d46f6a
Jacob Burenstam <burenstam@gmail.com>
2017-06-25 23:38:53 +0200

    Add API auth token functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :verify_api_auth_token
+
+  def verify_api_auth_token
+    env_token = ENV['API_AUTH_TOKEN']
+    return if env_token.blank?
+    return if params[:auth_token] == env_token
+
+    render json: { message: 'Invalid API auth token' }, status: :forbidden
+  end
 end
