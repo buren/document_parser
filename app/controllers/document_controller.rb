4ed1c2c5adb4b663271a6efb0532baf6431c4e50
Jacob Burenstam <burenstam@gmail.com>
2017-06-25 23:12:44 +0200

    Implement POST document/create action

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -1,4 +1,28 @@
+require 'yomu'
+
 class DocumentController < ApplicationController
   def create
+    file_param = document_params[:file]
+    decoded_file = Base64.decode64(file_param)
+    decoded_io = StringIO.new(decoded_file)
+
+    parsed_document = Yomu.new(decoded_io)
+    metadata = parsed_document.metadata
+
+    result = {
+      title: metadata['title'],
+      created_at: metadata['meta:creation-date'],
+      created_at_human: metadata['created'],
+      metadata: metadata,
+      text: parsed_document.text
+    }
+
+    render json: result
+  end
+
+  private
+
+  def document_params
+    params.permit(:file)
   end
 end
